9593fe1189e6598957a336eb0d47c2919e9e8e09
Guy Matz <gmatz@matz.org>
2012-12-22 12:06:08 -0500

    Initial Checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhereCarServer::Application.config.secret_token = '96c7655a11930461f26ede17676c867cbe044c5f7ec7952afcd24eb7d8cbfc67c5b6536e7058980b3b563533623ad5622586d323ccbb7a60a68f6aee02f6e43b'
