fa733f4b9797d313a8e3043f3478dd4e99a6b254
Guy Matz <gmatz@matz.org>
2012-12-22 17:43:27 -0500

    added relationships to model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,4 @@
 class Location < ActiveRecord::Base
   attr_accessible :gps, :user_id
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :passwd
+  has_many :locations
 end
